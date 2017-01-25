964efb2670cdcf9e6d795e43b2f256fab12603b2
wzc0x0 <wzc0x0@gmail.com>
2017-01-25 16:20:01 +0800

    Complete index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by WZC on 2017/1/18.
  */
+$(window).load(function () {
+    $.getJSON("image_config.json",function (json) {
+        for(let i = 0 ; i <json.length;i++){
+            $(".home_goods ul img").eq(i).attr({"src":json[i]["src"]})
+        }
+    })
+})
+$(function () {
+    $(".header").load("head.html")
+    $(".footer").load("foot.html")
+})
